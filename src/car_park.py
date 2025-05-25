cd91b533c29333da687387a68ddbc974ffba5a60
20086080 <20086080@tafe.wa.edu.au>
2025-05-25 10:18:21 +0530

    feat : Print Car_park location and capacity module added

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,11 @@
 class CarPark:
-    pass
+    def __init__(self, location = "Unknown", capacity = "Unknown",
+                 plates = None, sensors = None, displays = None):
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates or []      # empty list if value is None
+        self.sensors = sensors or []
+        self.displays = displays or []
+
+    def __str__(self):      # Print Car_park object
+        return f"Car Park at {self.location} , with {self.capacity} bays."
