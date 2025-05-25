f98ad56a803fe9064c1d9897a451ebb29ac08996
20086080 <20086080@tafe.wa.edu.au>
2025-05-25 10:28:41 +0530

    feat : Added a constructor and attributes to the display class

--- src/display.py
+++ src/display.py
@@ -1,2 +1,9 @@
 class Display:
-    pass
+    def __init__(self, id, car_park, message = "", is_on = False):
+        self.id = id
+        self.message = message
+        self.is_on = is_on
+        self.car_park = car_park
+
+    def __str__(self):      # Print display object
+        return f"Display {self.id} : Welcome to car park {self.car_park}."
