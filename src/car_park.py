d6ec4c773efc85685c96d959f225e513fc47db17
20086080 <20086080@tafe.wa.edu.au>
2025-05-25 15:27:36 +0530

    feat : Added available bays calculations

--- src/car_park.py
+++ src/car_park.py
@@ -2,13 +2,18 @@ from sensor import Sensor
 from display import Display
 
 class CarPark:
-    def __init__(self, location = "Unknown", capacity = "Unknown",
+    def __init__(self, location = "Unknown", capacity = 0,
                  plates = None, sensors = None, displays = None):
         self.location = location
         self.capacity = capacity
         self.plates = plates or []      # empty list if value is None
         self.sensors = sensors or []
         self.displays = displays or []
+
+    @property
+    def available_bays(self):
+        return self.capacity - len(self.plates) \
+            if len(self.plates) <  self.capacity else 0         # Check car_park not full
 
     def __str__(self):      # Print Car_park object
         return f"Car Park at {self.location} , with {self.capacity} bays."
