fd475ef28122c02cefb64b30f42c14b184e0e6e6
20086080 <20086080@tafe.wa.edu.au>
2025-05-31 17:56:53 +0530

    Feat : Main.py updated to create additional sensors and displays

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from car_park import CarPark
-from sensor import EntrySensor, ExitSensor, Sensor
+from sensor import EntrySensor, ExitSensor
 from display import Display
-
 
 # TODO: create a car park object with the location moondalup, capacity 100, and log_file "moondalup.txt"
 # TODO: create an entry sensor object with id 1, is_active True, and car_park car_park
@@ -10,14 +9,12 @@ from display import Display
 # TODO: drive 10 cars into the car park (must be triggered via the sensor - NOT by calling car_park.add_car directly)
 # TODO: drive 2 cars out of the car park (must be triggered via the sensor - NOT by calling car_park.remove_car directly)
 
-
-def update_car_park(plate):
-    pass
-
 car_park = CarPark("moondalup",100,None,[],[])
 
 entry_sensor = EntrySensor(1, car_park, True)
 exit_sensor = ExitSensor(2, car_park, True)
+entry_sensor1 = EntrySensor(3, car_park, True)
+exit_sensor1 = ExitSensor(2, car_park, False)
 
 display_one = Display("1",car_park,"Welcome to Moondalup",True)
 display_two = Display("2",car_park,"Welcome to Moondalup",True)
@@ -25,6 +22,8 @@ display_three = Display("3",car_park,"Welcome to Moondalup",False)
 
 car_park.register(entry_sensor)
 car_park.register(exit_sensor)
+car_park.register(entry_sensor1)
+car_park.register(exit_sensor1)
 car_park.register(display_one)
 car_park.register(display_two)
 car_park.register(display_three)
